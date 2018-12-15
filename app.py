adcb9cc4cc119cdc6768859a02ffe31edebd0927
Anushree Agrawal <anushree.agrawal27@gmail.com>
2018-12-15 14:30:39 -0500

    fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> bla<br/>"
-    return html.format(name="friend"))
+    return html.format(name="friend")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)

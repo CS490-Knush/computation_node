ee5bbb7bbf8a5732abfa6477948849b0b93a0049
Anushree Agrawal <anushree.agrawal27@gmail.com>
2018-12-15 14:26:35 -0500

    Fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> bla<br/>" \
+           "<b>Hostname:</b> bla<br/>"
     return html.format(name="friend"))
 
 if __name__ == "__main__":

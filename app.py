be6cbe31ff3dd02da0e17e98a075db65472bff10
Anushree Agrawal <anushree.agrawal27@gmail.com>
2018-12-15 22:33:08 -0500

    App.py updates

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import socket
 import time
 import subprocess
 from multiprocessing import Process
+import requests
 
 app = Flask(__name__)
 
@@ -33,19 +34,24 @@ def run_job():
     job = request.json
     data_file = job['data_file']
     spark_program = job['spark_program']
-    curr_time = time.time()
+    start_time = time.time()
     print("Starting to run spark program %s with file %s" % (spark_program, data_file))
-    p = subprocess.Popen(["scp", "root@%s:/%s" % (storage_node, data_file), "."])
-    sts = p.wait()
-    if sts is not 0:
-        # failure
-        return Response("File transfer did not work", status=400)
+    r = requests.get("http://%s/get_data_file/%s" % (storage_node, data_file))
+    print(r.content)
+    with open(data_file, 'wb') as f:
+        f.write(r.content)
+    # r = subprocess.Popen(["scp", "root@%s:/%s" % (storage_node, data_file), "."])
     p = Process(target=run_spark_job, args=(spark_program, data_file))
+    p.start()
     p.join()
     end_time = time.time()
     print("Finished running spark job %s with file %s" % (spark_program, data_file))
     print("Time: %d" % end_time-start_time)
     return Response("Success!", status=200)
+
+@app.route("/get_data_file/<filename>", methods=["GET"])
+def get_data_file(filename):
+    return app.send_static_file(filename)
 
 def run_spark_job(spark_program, data_file):
     try:
